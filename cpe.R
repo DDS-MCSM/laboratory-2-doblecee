70a53251bf8708e26df6f323b003f039cca3f6ac
pablo cid ramos <pablocidramos@gmail.com>
2019-01-12 20:39:23 +0100

    first approach

--- cpe.R
+++ cpe.R
@@ -6,34 +6,43 @@
 #                                                                              #
 #******************************************************************************#
 
-# install.packages("xml2")
-# library(xml2)
+install.packages("xml2",repos='http://cran.us.r-project.org')
+install.packages("rvest",repos='http://cran.us.r-project.org')
 
-# compressed_cpes_url <- "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.zip"
-# cpes_filename <- "cpes.zip"
-# download.file(compressed_cpes_url, cpes_filename)
-# unzip(zipfile = cpes_filename)
+library(xml2)
+library(rvest)
+
+compressed_cpes_url <- "https://nvd.nist.gov/feeds/xml/cpe/dictionary/official-cpe-dictionary_v2.3.xml.zip"
+cpes_filename <- "cpes.zip"
+download.file(compressed_cpes_url, cpes_filename)
+unzip(zipfile = cpes_filename)
 cpe.file <- "./official-cpe-dictionary_v2.3.xml"
 
 GetCPEItems <- function(cpe.raw) {
-  cpe <- NewCPEItem()
-  cpe.raw <- xml2::xml_find_all(cpe.raw)
+  #cpe <- NewCPEItem()
 
   # transform the list to data frame
+  cpe.reference <- rvest::html_text(rvest::xml_nodes(cpe.raw,xpath="//d1:cpe-item/d1:references/d1:reference/@href"))
+  cpe.title <- rvest::html_text(rvest::xml_nodes(cpe.raw,xpath="//d1:cpe-item/d1:title"))
+  cpe.id <- rvest::html_text(rvest::xml_nodes(cpe.raw,xpath="//d1:cpe-item/@name"))
 
   # return data frame
-}
-
-CleanCPEs <- function(cpes){
+  aux.data.frame <- data.frame(title=cpe.title
+                              )
 
   # data manipulation
 
-  return(data.frame())
+  return(aux.data.frame)
+}
+
+CleanCPEs <- function(cpes){
+  return(cpes)
 }
 
 ParseCPEData <- function(cpe.file) {
 
   # load cpes as xml file
+  cpes <- tempfile(fileext = ".xml")
   cpes <- xml2::read_xml(x = cpe.file)
 
   # get CPEs
